3bf87425465866f8ab79822437f4886a23163983
CcooLcyy <mutex_@outlook.com>
2024-01-22 23:48:29 +0800

    关闭自动换行

--- lua/my/options.lua
+++ lua/my/options.lua
@@ -20,11 +20,11 @@ vim.opt.numberwidth = 4
 vim.opt.updatetime = 100
 
 vim.opt.termguicolors = true
-vim.opt.wrap = true
+vim.opt.wrap = false
 
 vim.opt.cursorline =  true
 
 vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
 
-vim.api.nvim_command(':filetype plugin on')
+-- vim.api.nvim_command(':filetype plugin on')
