538c406cfb804176f85136cc80576a2201b48343
CcooLcyy <mutex_@outlook.com>
2024-01-23 17:30:41 +0800

    配置lualine插件

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,35 @@
+local lualine = {
+	"nvim-lualine/lualine.nvim",
+	dependencies = "nvim-tree/nvim-web-devicons",
+	config = function()
+		require("lualine").setup({
+			options = {
+				theme = "auto",
+				component_separators = { left = "|", right = "|" },
+				-- https://github.com/ryanoasis/powerline-extra-symbols
+				section_separators = { left = " ", right = "" },
+			},
+			extensions = { "nvim-tree", "toggleterm" },
+			sections = {
+				lualine_c = {
+					"filename",
+				},
+				lualine_x = {
+					"filesize",
+					{
+						"fileformat",
+						symbols = {
+							unix = '', -- e712
+							dos = '', -- e70f
+							mac = "", -- e711
+						},
+					},
+					"encoding",
+					"filetype",
+				},
+			},
+		})
+	end
+}
+
+return lualine
