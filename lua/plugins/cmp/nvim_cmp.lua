7fcfa8ee96e5e04a1c0630f65b864076b97e37c1
CcooLcyy <mutex_@outlook.com>
2024-01-24 17:42:23 +0800

    开启补全，并且使用tab进行å选择切换

--- lua/plugins/cmp/nvim_cmp.lua
+++ lua/plugins/cmp/nvim_cmp.lua
@@ -2,6 +2,12 @@ local nvim_cmp = {
     "hrsh7th/nvim-cmp",
     lazy = false,
     config = function()
+        local has_words_before = function()
+            local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+            return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+        end
+        local cmp = require("cmp")
+        local luasnip = require("luasnip")
         require("cmp").setup({
             snippet = {
                 -- REQUIRED - you must specify a snippet engine
@@ -22,8 +28,30 @@ local nvim_cmp = {
                 ['<C-Space>'] = require("cmp").mapping.complete(),
                 ['<C-e>'] = require("cmp").mapping.abort(),
                 -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-                ['<CR>'] = require("cmp").mapping.confirm({ select = true }), 
+                ['<CR>'] = require("cmp").mapping.confirm({ select = true }),
+                ["<Tab>"] = require("cmp").mapping(function(fallback)
+                    if require("cmp").visible() then
+                        require("cmp").select_next_item()
+                    elseif require("luasnip").expand_or_jumpable() then
+                        require("luasnip").expand_or_jump()
+                    elseif has_words_before() then
+                        cmp.complete()
+                    else
+                        fallback()
+                    end
+                end, { "i", "s" }),
+
+                ["<S-Tab>"] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        cmp.select_prev_item()
+                    elseif luasnip.jumpable(-1) then
+                        luasnip.jump(-1)
+                    else
+                        fallback()
+                    end
+                end, { "i", "s" }),
             }),
+
             sources = require("cmp").config.sources({
                 { name = 'nvim_lsp' },
                 -- { name = 'vsnip' }, -- For vsnip users.
