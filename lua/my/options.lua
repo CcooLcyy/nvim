f1582aa6cc608a69ec3fc8f4c7528b01b1a788a8
CcooLcyy <mutex_@outlook.com>
2024-01-24 21:47:08 +0800

    设置行数过长在下一行显示。 主要用在md文件中，在源码中应该分行写

--- lua/my/options.lua
+++ lua/my/options.lua
@@ -14,17 +14,17 @@ vim.opt.fileencoding = "UTF-8"
 vim.opt.autoread = true
 
 vim.opt.number = true
-vim.opt.relativenumber = true 
+vim.opt.relativenumber = true
 vim.opt.numberwidth = 4
 
 vim.opt.updatetime = 100
 
 vim.opt.termguicolors = true
-vim.opt.wrap = false
+vim.opt.wrap = true
 
 vim.opt.cursorline =  true
 
 vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
 
--- vim.api.nvim_command(':filetype plugin on')
+vim.api.nvim_command(':filetype plugin on')
